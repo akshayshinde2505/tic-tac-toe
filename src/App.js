f64bbebd44c7b272a226441209750347392e5c86
akshayshinde2505 <akshay.shinde@sysnerzip.com>
2018-08-02 18:43:35 +0530

    Removed unwanted files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Game from './component/Game';
 
@@ -7,11 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">GAME - TIC TAC TOE</h1>
-        </header>
-        <p className="App-intro"></p>
+        <p className="App-intro">TIC TAC TOE</p>
         <div className="App-intro">
           <Game />
         </div>
